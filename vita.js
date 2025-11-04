1eed4789e762883a702720c0edd412c156aac8a6
mio-nic <115370136+mio-nic@users.noreply.github.com>
2025-11-04 22:15:13 +0100

    Update vita.js

--- vita.js
+++ vita.js
@@ -21,7 +21,7 @@ const THRESHOLDS = {
     },
     // Freddo (Temp Min)
     'COLD': {
-        RED: -5, ORANGE: 0, YELLOW: 4,
+        RED: -5, ORANGE: 0, YELLOW: 6,
         title: 'Freddo', unit: '°C',
         desc: {
             4: 'Freddo Estremo. RISCHIO GELO, DANNI STRUTTURALI e ipotermia grave.',
@@ -661,6 +661,7 @@ export function getComfortChartInstance() {
 
 
 export const updateVisuals = updateComfortTable;
+
 
 
 
